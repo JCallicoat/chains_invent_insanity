87475bf1e09e33b9f3cf2c2695df6893666c1b74
JCallicoat <jordan.callicoat@gmail.com>
2017-02-07 01:00:55 -0600

    Comment showing how the wordlists were generated from yaml

--- wordlist_from_yaml.py
+++ wordlist_from_yaml.py
@@ -3,6 +3,8 @@
 
 # ls -Q dah-cards/ | fgrep -v -e Reject -e Ladies -e README -e scripts | xargs python wordlist_from_yaml.py > custom_wordlist.txt
 
+# python wordlist_from_yaml.py Base.yaml Box.yaml CAHe1.yaml CAHe2.yaml CAHe3.yaml CAHe4.yaml CAHe5.yaml CAHe6.yaml CAHgrognards.yaml CAHweeaboo.yaml CAHxmas.yaml christmas2013.yaml HACK.yaml NEIndy.yaml NSFH.yaml PAXE13.yaml PAXE14PP.yaml PAXE14.yaml PAXP13.yaml PAXP14PP.yaml PAXP15FP.yaml www.yaml > fun_wordlist.txt
+
 import sys
 import yaml
 
